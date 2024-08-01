e335d8f56920f74c34cfa9b5fdd4e96cd5e3a2c0
rroggenk <rroggenk@calpoly.edu>
2024-08-01 12:18:00 -0700

    connecting functions to shiny app + added contact info

--- NHG Shiny App/Negative_Hypergeometric_Confidence_Intervals/app.R
+++ NHG Shiny App/Negative_Hypergeometric_Confidence_Intervals/app.R
@@ -1,3 +1,8 @@
+# TO DO: 
+# make it so that for the inputs it only allows: M <= N, m<=M, and observed x <= N-m
+# make it so when the inputs are empty, it doenst crash, just has an error
+
+
 #
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
@@ -10,19 +15,17 @@
 library(shiny)
 library(tidyverse)
 library(extraDistr)
+library(DT)  # Load DT for data tables
 
+source('../../functions.R', encoding = 'UTF-8')
 
-source('functions.R', encoding = 'UTF-8')
-
-
-
-# Define UI for application that draws a histogram
+# Define UI for application that calculates and displays confidence intervals
 ui <- fluidPage(
   
   # Application title
   titlePanel("Confidence Intervals for the Negative Hypergeometric Distribution"),
   
-  # Sidebar with a slider input for number of bins 
+  # Sidebar with inputs
   sidebarLayout(
     sidebarPanel(
       selectInput(inputId = "parameter_of_interest", 
@@ -32,7 +35,8 @@ ui <- fluidPage(
       
       selectInput(inputId = "procedure", 
                   label = "Procedure", 
-                  choices = c("Normal Approximation / Large Sample", 
+                  choices = c("Normal Approximation / Large Sample (MLE)", 
+                              "Normal Approximation / Large Sample (Unbiased)",
                               "Analog to the Clopper-Pearson", 
                               "Modified Sterne", 
                               "Crow & Gardner", 
@@ -49,7 +53,7 @@ ui <- fluidPage(
       
       numericInput(inputId = "m", 
                    label = "Fixed number of successes to be observed (m)", 
-                   value = 1, 
+                   value = 5, 
                    min = 1, 
                    step = 1),
       
@@ -57,7 +61,7 @@ ui <- fluidPage(
         condition = "input.parameter_of_interest == 'N'",
         numericInput(inputId = "M", 
                      label = "Number of successes in the population (M)", 
-                     value = 0, 
+                     value = 10, 
                      min = 0, 
                      step = 1)
       ),
@@ -66,29 +70,37 @@ ui <- fluidPage(
         condition = "input.parameter_of_interest == 'M'",
         numericInput(inputId = "N", 
                      label = "Total number of items (N)", 
-                     value = 1, 
+                     value = 20, 
                      min = 1, 
                      step = 1)
       ),
       
       numericInput(inputId = "x", 
                    label = "Observed value of x (Number of failures observed before the mth success)", 
-                   value = 0, 
+                   value = 5, 
                    min = 0, 
                    step = 1),
       
       # Placeholder for error messages
-      textOutput("error_message")
+      textOutput("error_message"),
+      
+      # Contact information
+      HTML('<p style="font-size: x-small; margin-top: 20px;">
+             <strong>Contact Information:</strong><br>
+             Rachel Roggenkemper<br>
+             rroggenk@calpoly.edu
+           </p>')
     ),
     
-    # Show a plot of the generated distribution
+    # Main panel to show the text and data table
     mainPanel(
-      plotOutput("distPlot")
+      verbatimTextOutput("text_output"),  # For the descriptive text
+      DTOutput("result_table")  # For the interactive data table
     )
   )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic to calculate and display the confidence intervals
 server <- function(input, output, session) {
   
   observe({
@@ -96,6 +108,9 @@ server <- function(input, output, session) {
     if (input$confidence_level < 0 || input$confidence_level > 100) {
       updateNumericInput(session, "confidence_level", value = 95)
       output$error_message <- renderText("Error: Confidence Level must be between 0 and 100.")
+    } else if (input$confidence_level != floor(input$confidence_level)) {
+      updateNumericInput(session, "confidence_level", value = 95)
+      output$error_message <- renderText("Error: Confidence Level must be an integer.")
     } else {
       output$error_message <- renderText("")
     }
@@ -104,6 +119,9 @@ server <- function(input, output, session) {
     if (input$m < 1) {
       updateNumericInput(session, "m", value = 1)
       output$error_message <- renderText("Error: m must be an integer greater than or equal to 1.")
+    } else if (input$m != floor(input$m)) {
+      updateNumericInput(session, "m", value = 1)
+      output$error_message <- renderText("Error: m must be an integer.")
     } else {
       output$error_message <- renderText("")
     }
@@ -112,6 +130,9 @@ server <- function(input, output, session) {
     if (input$N < 1) {
       updateNumericInput(session, "N", value = 1)
       output$error_message <- renderText("Error: N must be an integer greater than or equal to 1.")
+    } else if (input$N != floor(input$N)) {
+      updateNumericInput(session, "N", value = 1)
+      output$error_message <- renderText("Error: N must be an integer.")
     } else {
       output$error_message <- renderText("")
     }
@@ -120,6 +141,9 @@ server <- function(input, output, session) {
     if (input$M < 0) {
       updateNumericInput(session, "M", value = 0)
       output$error_message <- renderText("Error: M must be an integer greater than or equal to 0.")
+    } else if (input$M != floor(input$M)) {
+      updateNumericInput(session, "M", value = 0)
+      output$error_message <- renderText("Error: M must be an integer.")
     } else {
       output$error_message <- renderText("")
     }
@@ -128,23 +152,83 @@ server <- function(input, output, session) {
     if (input$x < 0) {
       updateNumericInput(session, "x", value = 0)
       output$error_message <- renderText("Error: x must be an integer greater than or equal to 0.")
+    } else if (input$x != floor(input$x)) {
+      updateNumericInput(session, "x", value = 0)
+      output$error_message <- renderText("Error: x must be an integer.")
     } else {
       output$error_message <- renderText("")
     }
   })
   
-  output$distPlot <- renderPlot({
-    # Generate bins based on input$bins from ui.R
-    x <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # Draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white',
-         xlab = 'Waiting time to next eruption (in mins)',
-         main = 'Histogram of waiting times')
+  output$text_output <- renderText({
+    # Handle only if the parameter of interest is "M"
+    if (input$parameter_of_interest == "M") {
+      # Convert confidence level to a decimal
+      conf_level <- input$confidence_level / 100
+      
+      # Get N, m, and M based on the input
+      N <- input$N
+      m <- input$m
+      M <- input$M
+      
+      # Text output with parameter details
+      paste(
+        "Parameter of Interest:", input$parameter_of_interest, "\n",
+        "Procedure:", input$procedure, "\n",
+        "Confidence Level (%):", input$confidence_level, "\n",
+        "m (Number of successes to be observed):", input$m, "\n",
+        "N (Total number of items):", N, "\n",
+        "Observed x (Number of failures observed):", input$x, "\n"
+      )
+    } else {
+      "Currently only handling cases where the parameter of interest is M."
+    }
+  })
+  
+  output$result_table <- renderDT({
+    if (input$parameter_of_interest == "M") {
+      # Convert confidence level to a decimal
+      conf_level <- input$confidence_level / 100
+      
+      # Get N, m, and M based on the input
+      N <- input$N
+      m <- input$m
+      M <- input$M
+      
+      # Calculate confidence interval based on the selected procedure
+      result_df <- switch(input$procedure,
+                          "Normal Approximation / Large Sample (MLE)" = CI_cov_prob_MLE(N = N, m = m, conf_level = conf_level),
+                          "Normal Approximation / Large Sample (Unbiased)" = CI_cov_prob_unbiased(N = N, m = m, conf_level = conf_level),
+                          "Analog to the Clopper-Pearson" = CI_cov_prob(N = N, m = m, conf_level = conf_level),
+                          "Modified Sterne" = minimal_cardinality_ci(N = N, m = m, conf_level = conf_level, procedure = "MST"),
+                          "Crow & Gardner" = minimal_cardinality_ci(N = N, m = m, conf_level = conf_level, procedure = "CG"),
+                          "Bryne and Kabaila" = minimal_cardinality_ci(N = N, m = m, conf_level = conf_level, procedure = "BK"),
+                          "Blaker" = blaker_ci(N = N, m = m, conf_level = conf_level),
+                          "CMC" = cmc_ci(N = N, m = m, conf_level = conf_level)
+      )
+      
+      # Extract the row corresponding to the observed x
+      result_row <- result_df %>% filter(x == input$x)
+      
+      if (nrow(result_row) > 0) {
+        # Display the result in a data table
+        result_row %>%
+          datatable(rownames = FALSE, options = list(pageLength = 5, dom = 't', autoWidth = TRUE))
+      } else {
+        data.frame(Message = "No result found for the given inputs.") %>%
+          datatable(rownames = FALSE, options = list(pageLength = 1, dom = 't', autoWidth = TRUE))
+      }
+    } else {
+      data.frame(Message = "Currently only handling cases where the parameter of interest is M.") %>%
+        datatable(rownames = FALSE, options = list(pageLength = 1, dom = 't', autoWidth = TRUE))
+    }
   })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
+
+
 
